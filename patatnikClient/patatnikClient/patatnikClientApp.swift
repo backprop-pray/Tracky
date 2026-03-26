f03cf003b4652cd56c98fa8972ec1ca57f809d45
anastassow <127148449+anastassow@users.noreply.github.com>
2026-03-26 11:34:42 +0200

    backend login jwt auth and init of the swift client app

---
+++ patatnikClient/patatnikClient/patatnikClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  patatnikClientApp.swift
+//  patatnikClient
+//
+//  Created by Dimitar Anastassov on 26.03.26.
+//
+
+import SwiftUI
+
+@main
+struct patatnikClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
